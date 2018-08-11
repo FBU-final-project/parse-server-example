350467105b8ceb9b9c25273824c7fa0eadf197f5
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-08-10 18:12:04 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,21 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define("pushsample", function (request, response) {
+    Parse.Push.send({
+            channels: ["News"],
+            data: {
+                title: "Hello from the Cloud Code",
+                alert: "Back4App rocks!",
+            }
+       }, {
+            success: function () {
+                // Push was successful
+                response.success("push sent");
+                console.log("Success: push sent");
+            },
+            error: function (error) {
+                // Push was unsucessful
+                response.error("error with push: " + error);
+                console.log("Error: " + error);
+            },
+            useMasterKey: true
+       });
 });
